3fc3dde146b51f63a5a20d912cb7ef1334c98d90
jhon11390 <meljhon2@gmail.com>
2020-07-27 02:15:23 -0500

    se modifica app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/products", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/mongo-1", { useNewUrlParser: true })
 
 // definimos el schema
 const ProductSchema = new mongoose.Schema({
